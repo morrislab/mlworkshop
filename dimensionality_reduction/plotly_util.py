b5a320547465906cb0207b34f03a6b1d07db983e
ivan-krukov <ivan.o.kryukov@gmail.com>
2016-05-12 11:53:36 -0600

    finished dimensionality reduction

--- dimensionality_reduction/plotly_util.py
+++ dimensionality_reduction/plotly_util.py
@@ -6,11 +6,12 @@ default_marker = go.Marker(size=3, color="#006d2c")
 
 
 def scatter_matrix(M, marker=default_marker,
-                   width=800, height=800,
-                   title=None, x_label=None, y_label=None):
+                   width=600, height=600,
+                   title=None, x_label=None, y_label=None,
+                   dims=[0,1]):
     data = [go.Scatter(
-        x=M[:, 0],
-        y=M[:, 1],
+        x=M[:, dims[0]],
+        y=M[:, dims[1]],
         mode='markers',
         marker=marker
     )]
@@ -31,7 +32,7 @@ def scatter_matrix(M, marker=default_marker,
 
 
 def scatter_matrix_3d(M, marker=default_marker,
-                      width=800, height=800,
+                      width=600, height=600,
                       title=None, x_label=None, y_label=None, z_label=None):
     data = [go.Scatter3d(
         x=M[:, 0],
--- dimensionality_reduction/umbrella.py
+++ dimensionality_reduction/umbrella.py
@@ -10,17 +10,18 @@ class Umbrella:
         t = 2 * np.pi * random.uniform(size=n)
         u = random.uniform(size=n) + random.uniform(size=n)
         r = np.vectorize(lambda u: 2 - u if u > 1 else u)(u)
-        return np.column_stack((r * np.cos(t), r * np.sin(t)))
+        return np.column_stack((r * np.cos(t) + 1, r * np.sin(t) + 1))
 
-    def __init__(self, n, handle_thinner=10):
-        canopy = np.column_stack((self.__sample_circle(n), np.repeat(0, n)))
-        handle = np.column_stack((np.repeat(0, n // handle_thinner),
-                                  np.repeat(0, n // handle_thinner),
-                                  -random.uniform(high=2,
+    def __init__(self, n, handle_length=2, handle_thinner=10):
+        canopy = np.column_stack((self.__sample_circle(n), np.repeat(handle_length, n)))
+        handle = np.column_stack((np.repeat(1, n // handle_thinner),
+                                  np.repeat(1, n // handle_thinner),
+                                  random.uniform(high=handle_length,
                                                   size=n // handle_thinner)))
         self.matrix = np.row_stack((canopy, handle))
+        self.handle_length = handle_length
         self.color = np.apply_along_axis(
-            lambda row: "#d03" if row[2] == 0 else "#444", 1, self.matrix)
+            lambda row: "#d03" if row[2] == self.handle_length else "#444", 1, self.matrix)
         self.marker = go.Marker(
             size=4,
             color=self.color
